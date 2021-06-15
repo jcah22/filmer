b1861a0917b679f05e1eb5044661cb27fb1e5889
julio <jcah549@gmail.com>
2021-06-15 13:54:11 -0500

    primer commit del proyecto

---
+++ src/main/java/com/jcah/FilmerApplication.java
@@ -0,0 +1,13 @@
+package com.jcah;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilmerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jcah/FilmerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jcah;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
