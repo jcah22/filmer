b8638467c3b00d7cf7de4338f8133c1b60a41dd6
julio <jcah549@gmail.com>
2021-06-16 09:14:58 -0500

    terminando video 04

--- src/main/java/com/jcah/controllers/AdminController.java
+++ src/main/java/com/jcah/controllers/AdminController.java
@@ -1,24 +1,57 @@
 package com.jcah.controllers;
 
 import com.jcah.entities.Pelicula;
+import com.jcah.service.IPeliculasService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.UUID;
 
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
 
+    @Autowired
+    private IPeliculasService peliculasService;
+
+
     @GetMapping("/peli-form")
-    public String peliForm(Model model){
-        model.addAttribute("pelicula",new Pelicula());
+    public String peliForm(Model model) {
+        model.addAttribute("pelicula", new Pelicula());
         return "admin/peliForm";
     }
 
-    public String savePeli(){
+    @PostMapping("/save-peli")
+    public String savePeli(@RequestParam(name = "file") MultipartFile portada, Pelicula peli, RedirectAttributes mensajes) {
+        if (!portada.isEmpty()) {
+            String ruta = "C://Temp//uploads";
+            String nombreUnico = UUID.randomUUID() + " " + portada.getOriginalFilename();
+
+            try {
+                byte[] bytes = portada.getBytes();
+                Path rutaAbsoluta = Paths.get(ruta + "//" + nombreUnico);
+                Files.write(rutaAbsoluta, bytes);
+                peli.setPortada(nombreUnico);
+                peliculasService.save(peli);
+                mensajes.addFlashAttribute("peliculaGurdada", "Pelicula Guardada");
 
 
-        return "";
+            } catch (Exception e) {
+                e.getCause().getMessage();
+            }
+        }
+
+
+        return "redirect:/admin/peli-form";
     }
 }
